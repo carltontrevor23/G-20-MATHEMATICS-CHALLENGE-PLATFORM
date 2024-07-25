8650dbc4d9e13891a910b3823ba46d7d0bea6e19
carltontrevor23 <ayebarecarlton@gmail.com>
2024-07-26 01:03:42 +0300

    added CSS styles and JS
    addition of CSS Styles and JS used on the home page (dashboard)

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,13 +1,13 @@
-@extends('layouts.app', ['activePage' => 'user', 'title' => 'Light Bootstrap Dashboard Laravel by Creative Tim & UPDIVISION', 'navName' => 'User Profile', 'activeButton' => 'laravel'])
+@extends('layouts.app', ['activePage' => 'user', 'title' => 'Edit Profile', 'navName' => 'User Profile', 'activeButton' => 'laravel'])
 
 @section('content')
+<!--Editing details for the logged in user-->
     <div class="content">
         <div class="container-fluid">
             <div class="section-image">
-                <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
                 <div class="row">
 
-                    <div class="card col-md-8">
+                    <div class="card col-md-12">
                         <div class="card-header">
                             <div class="row align-items-center">
                                 <div class="col-md-8">
@@ -47,6 +47,7 @@
                                 </div>
                             </form>
                             <hr class="my-4" />
+                            <!--Changing password-->
                             <form method="post" action="{{ route('profile.password') }}">
                                 @csrf
                                 @method('patch')
@@ -81,46 +82,10 @@
                                     </div>
         
                                     <div class="text-center">
-                                        <button type="submit" class="btn btn-default mt-4">{{ __('Change password') }}</button>
+                                        <button type="submit" class="btn btn-default mt-4">{{ __('Change Password') }}</button>
                                     </div>
                                 </div>
                             </form>
-                        </div>
-                    </div>
-
-                    <div class="col-md-4">
-                        <div class="card card-user">
-                            <div class="card-image">
-                                <img src="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400" alt="...">
-                            </div>
-                            <div class="card-body">
-                                <div class="author">
-                                    <a href="#">
-                                        <img class="avatar border-gray" src="{{ asset('light-bootstrap/img/faces/face-3.jpg') }}" alt="...">
-                                        <h5 class="title">{{ __('Mike Andrew') }}</h5>
-                                    </a>
-                                    <p class="description">
-                                        {{ __('frankocean2928') }}
-                                    </p>
-                                </div>
-                                <p class="description text-center">
-                                {{ __(' "Lamborghini Mercy') }}
-                                    <br> {{ __('Your chick she so thirsty') }}
-                                    <br> {{ __('I am in that two seat Lambo') }}
-                                </p>
-                            </div>
-                            <hr>
-                            <div class="button-container mr-auto ml-auto">
-                                <button href="#" class="btn btn-simple btn-link btn-icon">
-                                    <i class="fa fa-facebook-square"></i>
-                                </button>
-                                <button href="#" class="btn btn-simple btn-link btn-icon">
-                                    <i class="fa fa-twitter"></i>
-                                </button>
-                                <button href="#" class="btn btn-simple btn-link btn-icon">
-                                    <i class="fa fa-google-plus-square"></i>
-                                </button>
-                            </div>
                         </div>
                     </div>
                 </div>
