19e80e2e814e982e6d26233b11b41dcd9c3ee442
carltontrevor23 <ayebarecarlton@gmail.com>
2024-07-26 00:17:59 +0300

    edits to dashboard.php and guest-view
    I added a guest view and also made edits to the dashboard.

--- app/Http/Controllers/GuestViewController.php
+++ app/Http/Controllers/GuestViewController.php
@@ -7,23 +7,18 @@ use App\Models\Challenges;
 use App\Models\School;
 use App\Models\Participants;
 
-class HomeController extends Controller
+class GuestViewController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
+    
     //method to display total registered schools, participants and challenges
     public function index()
     {
-
         $schoolsCount = School::count();
         $participantsCount = Participants::count();
         $challengesCount = Challenges::count();
 
         $upcoming_challenges = Challenges::where('start_date', '>', now())->get();
-        return view('dashboard', compact('upcoming_challenges', 'schoolsCount', 'participantsCount', 'challengesCount'));
+        return view('pages.guest-view', compact('upcoming_challenges', 'schoolsCount', 'participantsCount', 'challengesCount'));
     }
-
+    
 }
